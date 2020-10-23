527584643c2b50eeef3fc7c2f89bbe01e6b6f4c5
Carsten <carsten.draschner@gmail.com>
2020-10-23 20:30:29 +0200

    tryout also ontop

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/examples/QueryInML.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/examples/QueryInML.scala
@@ -1,10 +1,14 @@
 package net.sansa_stack.ml.spark.similarity.examples
 
+import net.sansa_stack.query.spark.ontop.OntopSPARQLEngine
 import org.apache.jena.riot.Lang
 import net.sansa_stack.rdf.spark.io._
-import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.{Row, SparkSession}
 import net.sansa_stack.query.spark.query._
+import net.sansa_stack.rdf.common.partition.core.{RdfPartitionComplex, RdfPartitionerComplex}
+import net.sansa_stack.rdf.spark.partition.core.RdfPartitionUtilsSpark
+import org.apache.jena.sparql.engine.binding.Binding
+import org.apache.spark.rdd.RDD
 
 object QueryInML {
 
@@ -17,13 +21,24 @@ object QueryInML {
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
 
-    val inputPath = "./sansa-ml/sansa-ml-spark/src/main/resources/movieData/movie.nt"
+    val inputPath = "./sansa-ml/sansa-ml-spark/src/main/resources/rdf.nt"
 
     val lang = Lang.NTRIPLES
     val graphRdd = spark.rdf(lang)(inputPath)
 
-    val sparqlQuery = "SELECT * WHERE {?s ?p ?o} LIMIT 10"
-    val result = graphRdd.sparql(sparqlQuery)
-    result.rdd.foreach(println)
+    // with sparqlfy
+    val sparqlQuery = "SELECT ?o WHERE {?s ?p ?o} LIMIT 10"
+    val result1 = graphRdd.sparql(sparqlQuery)
+    result1.rdd.foreach(println)
+
+    // with ontop
+    // apply vertical partitioning which is necessary for the current Ontop integration
+    val partitions: Map[RdfPartitionComplex, RDD[Row]] = RdfPartitionUtilsSpark.partitionGraph(graphRdd, partitioner = RdfPartitionerComplex(false))
+    val ontopEngine = OntopSPARQLEngine(spark, partitions, ontology = None)
+    val result2: RDD[Binding] = ontopEngine.execSelect(sparqlQuery)
+    result2.foreach(println)
+    for (b <- result2) {
+      println(b.get((b.vars()).next()))
+    }
   }
 }
