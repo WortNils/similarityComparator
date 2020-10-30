4103e5590a3d7df753835c3bdc58a5919ff1617a
Carsten <carsten.draschner@gmail.com>
2020-10-30 11:06:39 +0100

    started with ideas to use query in ml

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/examples/QueryInML.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/similarity/examples/QueryInML.scala
@@ -2,13 +2,15 @@ package net.sansa_stack.ml.spark.similarity.examples
 
 import net.sansa_stack.query.spark.ontop.OntopSPARQLEngine
 import org.apache.jena.riot.Lang
-import net.sansa_stack.rdf.spark.io._
 import org.apache.spark.sql.{Row, SparkSession}
 import net.sansa_stack.query.spark.query._
 import net.sansa_stack.rdf.common.partition.core.{RdfPartitionComplex, RdfPartitionerComplex}
 import net.sansa_stack.rdf.spark.partition.core.RdfPartitionUtilsSpark
 import org.apache.jena.sparql.engine.binding.Binding
 import org.apache.spark.rdd.RDD
+import net.sansa_stack.ml.spark.featureExtractor.SparqlFeatureExtractorModel
+import net.sansa_stack.rdf.spark.io._
+import net.sansa_stack.rdf.spark.model._
 
 object QueryInML {
 
@@ -21,15 +23,36 @@ object QueryInML {
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
 
+    // val inputPath = "./sansa-ml/sansa-ml-spark/src/main/resources/movieData/movie.nt"
     val inputPath = "./sansa-ml/sansa-ml-spark/src/main/resources/rdf.nt"
+    val lmdbPath = "/Users/carstendraschner/sampleDataSets/lmdb/linkedmdb-latest-dump.nt"
+
+    val ds = spark.read.rdf(Lang.NTRIPLES)(lmdbPath).toDS()
+
+    val fe = new SparqlFeatureExtractorModel()
+      .setQuery("SELECT ?o WHERE {?s ?p ?o} LIMIT 10")
+
+    // fe.transform(ds)
+
+    // assert(false)
+
 
     val lang = Lang.NTRIPLES
     val graphRdd = spark.rdf(lang)(inputPath)
 
     // with sparqlfy
     val sparqlQuery = "SELECT ?o WHERE {?s ?p ?o} LIMIT 10"
-    val result1 = graphRdd.sparql(sparqlQuery)
+
+   /* val result1 = graphRdd.sparql(sparqlQuery)
     result1.rdd.foreach(println)
+
+    for (res <- result1) {
+      println(res)
+    }
+
+    */
+
+
 
     // with ontop
     // apply vertical partitioning which is necessary for the current Ontop integration
@@ -40,5 +63,9 @@ object QueryInML {
     for (b <- result2) {
       println(b.get((b.vars()).next()))
     }
+
+    val tmpV = result2.collect()(0).vars().next()
+    val tmp: RDD[String] = result2.map(_.get(tmpV).toString())
+    tmp.foreach(println)
   }
 }
