6b5145468af2060866f0735fac260c145950cb18
Nils Wortmann <nilswo@gmail.com>
2021-01-25 01:50:36 +0100

    tweaks on Resnik

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/ResnikModel.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/ResnikModel.scala
@@ -14,6 +14,7 @@ import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.apache.spark.sql.functions._
 import net.sansa_stack.ml.spark.evaluation.utils.FeatureExtractorEval
+import org.apache.spark.sql.Row
 
 class ResnikModel extends GenericSimilarityModel {
   val spark = SparkSession.builder.getOrCreate()
@@ -22,14 +23,20 @@ class ResnikModel extends GenericSimilarityModel {
   private var _depth: Int = 1
   private var _outputCol: String = "extractedFeatures"
 
+  override val estimatorName: String = "ResnikSimilarityEstimator"
+  override val estimatorMeasureType: String = "similarity"
+
   override def transform(dataset: Dataset[_], target: DataFrame): DataFrame = {
+    val t0 = System.nanoTime()
     import spark.implicits._
     val featureExtractorModel = new FeatureExtractorEval()
       .setMode("par").setDepth(_depth)
     val parents = featureExtractorModel
       .transform(dataset, target)
 
-    target.map(row =>
+    val frame = target.withColumn("Resnik", lit(0)).withColumn("ResnikTime", lit(0))
+
+    frame.map(row: Row =>
       val a = parents.filter("uri" == row(0)).drop("uri").toDF
       val b = parents.filter("uri" == row(1)).drop("uri").toDF
       val common: DataFrame = a.intersect(b)
@@ -38,10 +45,11 @@ class ResnikModel extends GenericSimilarityModel {
       val informationContent = featureExtractorModel
       .transform(dataset, common)
       val resnik = informationContent.sort(desc(columnName = "extractedFeatures")).head()
-    )
+      val t1 = System.nanoTime()
+      val t_diff = t1-t0
+      row("Resnik") = resnik
+      row("ResnikTime") = t_diff
+      row
+    ).toDF
   }
-
-  override val estimatorName: String = "ResnikSimilarityEstimator"
-  override val estimatorMeasureType: String = "similarity"
-
 }
