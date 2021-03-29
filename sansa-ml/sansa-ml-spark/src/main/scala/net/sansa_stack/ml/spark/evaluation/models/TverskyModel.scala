a5f518d28fe0f2a3257969c15a97c785fabb5525
Nils Wortmann <nilswo@gmail.com>
2021-03-29 23:55:31 +0200

    first try tversky

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/TverskyModel.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/TverskyModel.scala
@@ -2,12 +2,13 @@ package net.sansa_stack.ml.spark.evaluation.models
 
 import net.sansa_stack.ml.spark.evaluation.utils.FeatureExtractorEval
 import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-import scala.collection.mutable.WrappedArray.ofRef
 
+import scala.collection.mutable.WrappedArray.ofRef
 import scala.collection.Map
 
 /**
@@ -27,11 +28,56 @@ class TverskyModel extends Transformer {
   private var _target: DataFrame = spark.emptyDataFrame
   private var _parents: DataFrame = spark.emptyDataFrame
 
+  private var _alpha: Double = 1.0
+  private var _beta: Double = 1.0
+
   private var _info: Map[String, Double] = Map(" " -> 0)
 
   val estimatorName: String = "TverskySimilarityEstimator"
   val estimatorMeasureType: String = "similarity"
 
+  protected val tversky = udf((a: Vector, b: Vector, alpha: Double, betha: Double) => {
+    val featureIndicesA = a.toSparse.indices
+    val featureIndicesB = b.toSparse.indices
+    val fSetA = featureIndicesA.toSet
+    val fSetB = featureIndicesB.toSet
+    if (fSetA.union(fSetB) == 0) {
+      0.0
+    }
+    else {
+      val tversky: Double = (
+        (fSetA.intersect(fSetB).size.toDouble)/
+          (
+            (fSetA.intersect(fSetB).size.toDouble)
+              +  (alpha * fSetA.diff(fSetB).size.toDouble)
+              + (betha * fSetB.diff(fSetA).size.toDouble)
+            )
+        )
+      tversky
+    }
+  })
+
+  def setAlpha(a: Double): this.type = {
+    if (a < 0 || a > 1) {
+      throw new Error("PROBLEM: alpha has to be between 0 and 1")
+    }
+    else {
+      _alpha = a
+      this
+    }
+  }
+
+  def setBeta(b: Double): this.type = {
+    if (b < 0 || b > 1) {
+      throw new Error("PROBLEM: alpha has to be between 0 and 1")
+    }
+    else {
+      _beta = b
+      this
+    }
+  }
+
+/*
   /**
    * This function takes a list of features for two entities a and b and returns their Tversky similarity value
    * and the time it took to calculate that value
@@ -50,6 +96,7 @@ class TverskyModel extends Transformer {
     /* a.keySet.intersect(b.keySet).map(k => k->(a(k),b(k))). */
     tverskyMethod(a.toList, b.toList)
   })
+ */
 
   /**
    * This method sets the iteration depth for the parent feature extraction
@@ -87,7 +134,7 @@ class TverskyModel extends Transformer {
 
     // parent calculation
     val featureExtractorModel = new FeatureExtractorEval()
-      .setMode("feat").setDepth(_depth)
+      .setMode("tvers").setDepth(_depth)
       .setTarget(_target.drop("entityA")
         .withColumnRenamed("entityB", "uri")
         .union(_target.drop("entityB")
@@ -97,15 +144,15 @@ class TverskyModel extends Transformer {
       .transform(dataset)
 
     val target = _target.join(features, _target("entityA") === features("entity"))
-      .withColumnRenamed("features", "featuresA")
+      .withColumnRenamed("vectorizedFeatures", "featuresA")
       .join(features, _target("entityB") === features("entity"))
-      .withColumnRenamed("features", "featuresB")
+      .withColumnRenamed("vectorizedFeatures", "featuresB")
 
     // timekeeping
     val t1 = System.nanoTime()
     t_net = t1 - t0
 
-    val result = target.withColumn("TverskyTemp", tversky(col("featuresA"), col("featuresB")))
+    val result = target.withColumn("TverskyTemp", tversky(col("featuresA"), col("featuresB"), lit(_alpha), lit(_beta)))
       .drop("featuresA", "featuresB")
     result.withColumn("Tversky", result("TverskyTemp._1"))
       .withColumn("TverskyTime", result("TverskyTemp._2"))
