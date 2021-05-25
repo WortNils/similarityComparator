ddead0a3cf68d49cc56c2da35c74f75ca69eb958
Nils Wortmann <nilswo@gmail.com>
2021-05-25 10:45:17 +0200

    fix comments lacking spaces

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/apsp/AllPairsShortestPath.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/apsp/AllPairsShortestPath.scala
@@ -13,42 +13,42 @@ import scala.concurrent.duration._
 
 private object AllPairsShortestPath {
   def main(args: Array[String]) {
-    //println(args.mkString(", "))
+    // println(args.mkString(", "))
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
     val conf = new SparkConf().setAppName("AllPairsShortestPath").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    //sc.setCheckpointDir("checkpoint/")
+    // sc.setCheckpointDir("checkpoint/")
     val n = 6
-    //val m = args(1).toInt
-    //val stepSize = args(2).toInt
-    //val interval = args(3).toInt
+    // val m = args(1).toInt
+    // val stepSize = args(2).toInt
+    // val interval = args(3).toInt
     val graph = generateGraph(n, sc)
     val apsp = new DistributedBlockFW
 
-    //val matA = generateInput(graph, n, sc, m, m)
-    //val ApspPartitioner = GridPartitioner(matA.numRowBlocks, matA.numColBlocks, matA.blocks.partitions.length)
+    // val matA = generateInput(graph, n, sc, m, m)
+    // val ApspPartitioner = GridPartitioner(matA.numRowBlocks, matA.numColBlocks, matA.blocks.partitions.length)
 
-    //val localMat = matA.toLocalMatrix()
-    //val resultMat = time{distributedApsp(matA, stepSize, ApspPartitioner, interval)}
+    // val localMat = matA.toLocalMatrix()
+    // val resultMat = time{distributedApsp(matA, stepSize, ApspPartitioner, interval)}
 
 
     val result = time {
       apsp.compute(graph, 3)
     }
-    //val resultMat1 = time{distributedApsp(matA, 1, ApspPartitioner, interval)}
+    // val resultMat1 = time{distributedApsp(matA, 1, ApspPartitioner, interval)}
     val resultLocalMat = result.toLocal()
-    //val resultLocalMat1 = resultMat1.toLocalMatrix()
+    // val resultLocalMat1 = resultMat1.toLocalMatrix()
     // println(fromBreeze(localMinPlus(toBreeze(localMat), toBreeze(localMat.transpose))).toString())
-    //println(matA.toLocalMatrix().toString())
-    //println(localMat.toString)
+    // println(matA.toLocalMatrix().toString())
+    // println(localMat.toString)
 
     println(resultLocalMat.toString)
     println()
     println(result.lookupDist(1, 2))
-    //println()
-    //println(resultLocalMat1.toString)
+    // println()
+    // println(resultLocalMat1.toString)
     // val collectedValues = blockMin(matA.blocks, matA.transpose.blocks, ApspPartitioner).foreach(println)
     // blockMinPlus(matA.blocks, matA.transpose.blocks, matA.numRowBlocks, matA.numColBlocks, ApspPartitioner).foreach(println)
     System.exit(0)
