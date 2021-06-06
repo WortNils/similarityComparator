9cbabbda8dff114d0682db29d16a1cc9542574e7
Nils Wortmann <nilswo@gmail.com>
2021-06-06 14:53:53 +0200

    reintroduce Generic Similarity Model

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/GenericSimilarityModel.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/GenericSimilarityModel.scala
@@ -10,12 +10,27 @@ import org.apache.spark.sql.functions.collect_list
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
+import scala.collection.Map
+
 
 /**
  * This class generates similarity measures for a given array of uri pairs.
  */
-class GenericSimilarityModel {
-/*
+class GenericSimilarityModel extends Transformer {
+  val spark = SparkSession.builder.getOrCreate()
+  import spark.implicits._
+  private val _availableModes = Array("res")
+  private var _mode: String = "res"
+  private var _depth: Int = 1
+  private var _outputCol: String = "extractedFeatures"
+
+  private var t_net: Double = 0.0
+
+  private var _target: DataFrame = spark.emptyDataFrame
+  private var _parents: DataFrame = spark.emptyDataFrame
+
+  private var _info: Map[String, Double] = Map(" " -> 0)
+
   // values that have to be overwritten
   protected var _similarityEstimationColumnName = "estimation"
 
@@ -23,17 +38,13 @@ class GenericSimilarityModel {
   val estimatorMeasureType: String = "path based, information content based, feature based or hybrid"
   val modelType: String = "SimilarityEstimator"
 
-  override def transform(dataset: Dataset[_], target: Array[(String, String)]): DataFrame = {
+  override def transform(dataset: Dataset[_]): DataFrame = {
     return dataset.toDF
   }
-
-
-
-  // TODO: fix unimplemented functions
   override def copy(extra: ParamMap): Transformer = defaultCopy(extra)
 
   override def transformSchema(schema: StructType): StructType =
     throw new NotImplementedError()
 
-  override val uid: String = "Similarity" */
+  override val uid: String = "Similarity"
 }
