09633621696e9a39412a3011c9b1ea8dcaad99d9
Nils Wortmann <nilswo@gmail.com>
2021-06-08 17:24:51 +0200

    add comments to SimilarityModel

--- sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/SimilarityModel.scala
+++ sansa-ml/sansa-ml-spark/src/main/scala/net/sansa_stack/ml/spark/evaluation/models/SimilarityModel.scala
@@ -3,19 +3,24 @@ package net.sansa_stack.ml.spark.evaluation.models
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 trait SimilarityModel {
+  // generic variables
   val spark = SparkSession.builder.getOrCreate()
   import spark.implicits._
+  private var t_net: Double = 0.0
+
+  // input dataframes
+  private var _target: DataFrame = spark.emptyDataFrame
+  private var _features: DataFrame = spark.emptyDataFrame
+
+  // meta information
+  val estimatorName: String = "SimilarityEstimator"
+  val estimatorMeasureType: String = "path based, information content based, feature based or hybrid"
+  val modelType: String = "SimilarityEstimator"
+
+  // model specific variables
   private val _availableModes = Array("")
   private var _mode: String = ""
 
   private var _inputCols: Array[String] = Array("uri", "features")
   private var _outputCol: String = "similarityValue"
-
-  private var t_net: Double = 0.0
-
-  private var _target: DataFrame = spark.emptyDataFrame
-  private var _features: DataFrame = spark.emptyDataFrame
-
-  val estimatorName: String = "SimilarityEstimator"
-  val estimatorMeasureType: String = "similarity"
 }
